54ded1e850e12c9c832202fa5060e1b642d012b5
KakukLevente <113351599+KakukLevente@users.noreply.github.com>
2023-10-05 14:27:24 +0200

    Add files via upload
    fixed login

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
+const loginButton = document.getElementById('LoginBtn');
 
 registerLink.addEventListener('click', ()=> {
     wrapper.classList.add('active');
@@ -18,4 +19,11 @@ btnPopup.addEventListener('click', ()=> {
 
 iconClose.addEventListener('click', ()=> {
     wrapper.classList.remove('active-popup');
+});
+
+loginButton.addEventListener('click', () => {
+  const checkbox = document.getElementById('check');
+  if (checkbox.checked) {
+    checkbox.checked = false;
+  }
 });
