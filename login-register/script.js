b5294af09880361d2704c7daeac27d1d09bd7bea
KakukLevente <113351599+KakukLevente@users.noreply.github.com>
2023-11-07 15:31:46 +0100

    Add files via upload

--- login-register/script.js
+++ login-register/script.js
@@ -1,16 +1,26 @@
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
+const back2LoginLink = document.querySelector('.back2login-link');
 const registerLink = document.querySelector('.register-link');
+const forgotPasswordLink = document.querySelector('.forgotPassword-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
 const loginButton = document.getElementById('LoginBtn');
+const checkbox = document.getElementById('check');
 
 registerLink.addEventListener('click', ()=> {
     wrapper.classList.add('active');
 });
 
+forgotPasswordLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active2');
+});
+
 loginLink.addEventListener('click', ()=> {
     wrapper.classList.remove('active');
+});
+back2LoginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active2');
 });
 
 btnPopup.addEventListener('click', ()=> {
@@ -22,7 +32,6 @@ iconClose.addEventListener('click', ()=> {
 });
 
 loginButton.addEventListener('click', () => {
-  const checkbox = document.getElementById('check');
   if (checkbox.checked) {
     checkbox.checked = false;
   }
